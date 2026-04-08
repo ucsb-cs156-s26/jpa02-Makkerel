d432b52ec8383267c92f1d3a293478263052583e
Kevin Zhao <kzhao132018@gmail.com>
2026-04-08 10:35:29 -0700

    team member change to real teammates

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -25,7 +25,6 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
         return "Makkerel";
     }
 
@@ -35,14 +34,13 @@ public class Developer {
      */
 
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-02");
+        team.addMember("Arman");
+        team.addMember("David Chen");
+        team.addMember("Kevin");
+        team.addMember("Kai");
+        team.addMember("Andrew B");
+        team.addMember("Justin");
         return team;
     }
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -33,6 +33,33 @@ public class DeveloperTest {
         assertEquals("Makkerel", Developer.getGithubId());
     }
 
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team t = Developer.getTeam();
+        assertEquals("s26-02", t.getName());
+        assertTrue(
+            t.getMembers().contains("Arman"),
+            "Team should contain Arman"
+        );
+        assertTrue(
+            t.getMembers().contains("David Chen"),
+            "Team should contain David Chen"
+        );
+        assertTrue(
+            t.getMembers().contains("Kevin"),
+            "Team should contain Kevin"
+        );
+        assertTrue(t.getMembers().contains("Kai"), "Team should contain Kai");
+        assertTrue(
+            t.getMembers().contains("Andrew B"),
+            "Team should contain Andrew B"
+        );
+        assertTrue(
+            t.getMembers().contains("Justin"),
+            "Team should contain Justin"
+        );
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 }
